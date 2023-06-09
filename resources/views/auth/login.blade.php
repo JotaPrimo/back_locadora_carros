230c3bd8903e813d51fb0a1fe8152681eea037a1
JotaPrimo <gestald118@gmail.com>
2023-06-09 12:37:05 -0400

    347. Vue Props - Encaminhando o token csrf para o componente Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <login-component></login-component>
+    {{-- props é uma forma de enviar parametros para o componente na forma de propriedades --}}
+    <login-component csrf_token="{{ @csrf_token()}}"></login-component>
 @endsection
