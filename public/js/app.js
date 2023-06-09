f67cfc4bfed0aec75d2e6457aebb397306425df5
JotaPrimo <gestald118@gmail.com>
2023-06-09 16:52:33 -0400

    354. Criando o card de listagem de registros

--- public/js/app.js
+++ public/js/app.js
@@ -5327,6 +5327,42 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   name: "Home"
@@ -27998,7 +28034,7 @@ var render = function () {
   var _c = _vm._self._c || _h
   return _c("div", { staticClass: "container" }, [
     _c("div", { staticClass: "row justify-content-center" }, [
-      _c("div", { staticClass: "col-md-8" }, [
+      _c("div", { staticClass: "col-md-10" }, [
         _c("div", { staticClass: "card" }, [
           _c("div", { staticClass: "card-header" }, [
             _vm._v("Busca de Marcas"),
@@ -28070,6 +28106,8 @@ var render = function () {
           _vm._v(" "),
           _vm._m(0),
         ]),
+        _vm._v(" "),
+        _vm._m(1),
       ]),
     ]),
   ])
@@ -28088,6 +28126,71 @@ var staticRenderFns = [
         },
         [_vm._v("Buscar")]
       ),
+    ])
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "card mt-3" }, [
+      _c("div", { staticClass: "card-header" }, [_vm._v("Listagem de Marcas")]),
+      _vm._v(" "),
+      _c("div", { staticClass: "card-body" }, [
+        _c("table", { staticClass: "table table-hover table-bordered" }, [
+          _c("thead", [
+            _c("tr", [
+              _c("th", { attrs: { scope: "col" } }, [_vm._v("#")]),
+              _vm._v(" "),
+              _c("th", { attrs: { scope: "col" } }, [_vm._v("First")]),
+              _vm._v(" "),
+              _c("th", { attrs: { scope: "col" } }, [_vm._v("Last")]),
+              _vm._v(" "),
+              _c("th", { attrs: { scope: "col" } }, [_vm._v("Handle")]),
+            ]),
+          ]),
+          _vm._v(" "),
+          _c("tbody", [
+            _c("tr", [
+              _c("th", { attrs: { scope: "row" } }, [_vm._v("1")]),
+              _vm._v(" "),
+              _c("td", [_vm._v("Mark")]),
+              _vm._v(" "),
+              _c("td", [_vm._v("Otto")]),
+              _vm._v(" "),
+              _c("td", [_vm._v("@mdo")]),
+            ]),
+            _vm._v(" "),
+            _c("tr", [
+              _c("th", { attrs: { scope: "row" } }, [_vm._v("2")]),
+              _vm._v(" "),
+              _c("td", [_vm._v("Jacob")]),
+              _vm._v(" "),
+              _c("td", [_vm._v("Thornton")]),
+              _vm._v(" "),
+              _c("td", [_vm._v("@fat")]),
+            ]),
+            _vm._v(" "),
+            _c("tr", [
+              _c("th", { attrs: { scope: "row" } }, [_vm._v("3")]),
+              _vm._v(" "),
+              _c("td", { attrs: { colspan: "2" } }, [_vm._v("Larry the Bird")]),
+              _vm._v(" "),
+              _c("td", [_vm._v("@twitter")]),
+            ]),
+          ]),
+        ]),
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "card-footer" }, [
+        _c(
+          "button",
+          {
+            staticClass: "float-end btn btn-primary btn-sm",
+            attrs: { type: "button" },
+          },
+          [_vm._v("Adicionar")]
+        ),
+      ]),
     ])
   },
 ]
--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-10">
                 <div class="card">
                     <div class="card-header">Busca de Marcas</div>
                     <div class="card-body">
@@ -36,16 +36,52 @@
                                         aria-describedby="nomeHelp">
 
                                 </input-container-component>
-
                             </div>
-
                         </div>
-
                     </div>
                     <div class="card-footer">
                         <button type="submit" class="float-end btn btn-primary btn-sm">Buscar</button>
                     </div>
                 </div>
+                <!--    inicio card listagem marcas -->
+                <div class="card mt-3">
+                    <div class="card-header">Listagem de Marcas</div>
+                    <div class="card-body">
+                        <table class="table table-hover table-bordered">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="card-footer">
+                        <button type="button" class="float-end btn btn-primary btn-sm">Adicionar</button>
+                    </div>
+                </div>
+                <!--    fim card listagem marcas -->
             </div>
         </div>
     </div>
